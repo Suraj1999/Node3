800bf89a8a801dcf0c31559b1206b5679bfe21db
Suraj1999 <102948555+Suraj1999@users.noreply.github.com>
2022-06-28 12:46:19 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+const users = [
+    {
+        id : "aaa",
+        login : "aaa@gmail.com",
+        password : "123",
+        age : 15,
+        isDeleted : false,
+    },
+    {
+        id : "aab",
+        login : "aab@gmail.com",
+        password : "123",
+        age : 18,
+        isDeleted : false,
+    },
+    {
+        id : "abb",
+        login : "abb@gmail.com",
+        password : "123",
+        age : 19,
+        isDeleted : false,
+    }
+];
+
+module.exports = {
+    users : users,
+}
